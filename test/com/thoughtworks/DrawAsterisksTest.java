a65c19df97b2552db5ccc32ab16b633c6b1874d2
Sailesh Dev <saileshdev@gmail.com>
2017-07-04 13:08:00 +0530

    Test to return expected asterisks

---
+++ test/com/thoughtworks/DrawAsterisksTest.java
@@ -0,0 +1,25 @@
+package com.thoughtworks;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+
+/**
+ * Created by saileshdev on 04/07/2017.
+ */
+public class DrawAsterisksTest {
+
+    private DrawAsterisks drawAsterisks;
+
+    @Before
+    public void setUp() throws Exception {
+        this.drawAsterisks = new DrawAsterisks();
+
+    }
+
+    @Test
+    public void shouldReturnSingleAsterisk(){
+        assertEquals(drawAsterisks.drawAsterisks(1),"*");
+    }
+
+}
