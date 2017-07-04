d9e0b8374dcbce1e159e706399e30678eb1afcc2
Sailesh Dev <saileshdev@gmail.com>
2017-07-04 15:33:34 +0530

    Update main method

--- src/com/thoughtworks/Main.java
+++ src/com/thoughtworks/Main.java
@@ -7,12 +7,20 @@ public class Main {
 	// write your code here
 
         PrintAsterisk printer = new PrintAsterisk();
+
+        System.out.println("Question 1: Print 1 asterisk to the console");
         printer.printSingleAsterisk();
         System.out.println();
 
-        printer.printHorizontalLine(6);
-
+        System.out.println("Question 2: Print n asterisk on one line to the console(Ex: n=8)");
+        printer.printHorizontalLine(8);
         System.out.println();
-        printer.printVerticalLine(2);
+
+        System.out.println("Question 3: Print vertical to the console(Ex: n=3)");
+        printer.printVerticalLine(3);
+        System.out.println();
+
+        System.out.println("Question 4: Print right triangle(Ex: n=3)");
+        printer.printRightTriangle(3);
     }
 }
